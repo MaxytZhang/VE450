8ccae2a75ce1d36687f9621134f7cc536468d364
Wendy Lyu <wendylyu@umich.edu>
2019-07-29 14:23:37 +0800

    Update temp.py

--- Back-end/Controllers/temp.py
+++ Back-end/Controllers/temp.py
@@ -24,5 +24,38 @@ def employee_login():
 
     #if no, then return does not match
 
-    #if yes, then return dashboard info
+    #if yes, then return publish dashboard
+
+@app.route('/backend/api/v1.0/more_notice', methods=['GET','POST'])
+def load_more_notices():
+    package = request.form
+    if not package["type"] == "notices":
+        pkg = {}
+        pkg = add_type("error",pkg)
+        pkg = add_message("Wrong package sent.", pkg)
+        return jsonify(pkg)
+    #find all future meetings of the current user, meeting_id -> attendee_id -> feedback
+
+
+
+@app.route('/backend/api/v1.0/meeting_history', methods=['GET','POST'])
+def request_history():
+    package = request.form
+    if not package["type"] == "history":
+        pkg = {}
+        pkg = add_type("error",pkg)
+        pkg = add_message("Wrong package sent.", pkg)
+        return jsonify(pkg)
+    #find all meeting histories of the current user
+
+@app.route('/backend/api/v1.0/check_meeting', methods=['GET','POST'])
+def check_meeting():
+    package = request.form
+    if not package["type"] == "check":
+        pkg = {}
+        pkg = add_type("error",pkg)
+        pkg = add_message("Wrong package sent.", pkg)
+        return jsonify(pkg)
+    meeting_id = package["meeting_id"]
+    #pull from the database
 
