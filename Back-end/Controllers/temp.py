1f87e38675415299e06c0f3ab503e6dce0a6e2dc
Wendy Lyu <wendylyu@umich.edu>
2019-07-29 13:27:58 +0800

    change

---
+++ Back-end/Controllers/temp.py
@@ -0,0 +1,11 @@
+#!flask/bin/python
+
+@app.route('/backend/api/v1.0/meeting_submit', methods=['GET','POST'])
+def submit_meeting():
+    package = request.form
+    if not package["type"] == "meeting":
+        return jsonify(make_package("error","error - sending wrong data"))
+    meeting_info = package
+    
+
+    
