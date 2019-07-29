c7f57a78d19bb7ceb01d7b91400605354ea1f321
reigetsu <37430941+reigetsu@users.noreply.github.com>
2019-07-29 14:31:59 +0800

    Update temp.py

--- Back-end/Controllers/temp.py
+++ Back-end/Controllers/temp.py
@@ -1,7 +1,7 @@
 #!flask/bin/python
 
 @app.route('/backend/api/v1.0/meeting_submit', methods=['GET','POST'])
-def submit_meeting():
+def submit_meeting(cursor):
     package = request.form
     if not package["type"] == "meeting":
         pkg = {}
@@ -9,6 +9,26 @@ def submit_meeting():
         pkg = add_message("Wrong package sent.", pkg)
         return jsonify(pkg)
     meeting_info = package
+    Insertquery = "INSERT INTO `meeting` (`MeetingID`, `Name`, `MeetingRooms`, `MeetingTopic`, `Date`, `StartTime`, `EndTime`, `Attendee`, `NeedHarware`, `Status`, `IsRoutine`, `Requires`, `Sites`, `Outline`, `Initiator`, `Memo`) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+    meetingid = package["id"]
+    meetingname = generate_name(package["id"])
+    meetingtopic = package["meeting_topic"]
+    meetingrooms = package["meeting_rooms"]
+    meetingdate = convert_date(int(package["start_timestamp"])/1000)
+    meetingst = convert_time(int(package["start_timestamp"])/1000)
+    meetinget = convert_time(int(package["end_timestamp"])/1000)
+    meetinghw = package["need_hw_support"]
+    meetingattendees = package["attendees"]
+    meetingroutine = package["is_routine"]
+    meetingrequires = package["need_hw_support"]
+    meetingsites = package["sites"]
+    meetingoutline = package["meeting_outline"]
+    meetinginitiator = package["initiator"]
+    meetingstatus = package["status"]
+    meetingmemo = package["meeting_memo"]
+    datatuple = (meetingid,meetingname,meetingrooms,meetingtopic,meetingdate,meetingst,meetinget,meetingattendees,meetinghw,meetingstatus,meetingroutine,meetingrequires,meetingsites,meetingoutline,meetinginitiator,meetingmemo)
+    cursor.execute(Insertquery,datatuple)
+    connection.commit()
 
 
 
