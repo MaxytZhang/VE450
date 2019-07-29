03f79fac42837ca00bd940536858febf352a3e54
Yangtao(Max) Zhang <maxzhang@umich.edu>
2019-07-29 13:17:41 +0800

    增加布置新会议成功反馈

--- Front-end/src/components/NewMeetingMain.vue
+++ Front-end/src/components/NewMeetingMain.vue
@@ -7,15 +7,6 @@
             <el-step title="Step 3"></el-step>
         </el-steps>
         <router-view></router-view>
-        <!--<el-row type="flex">-->
-            <!--<el-col :span="4">-->
-                <!--<el-button style="margin-top: 12px; float: left" @click="pre" v-show="show_pre">Previous</el-button>-->
-            <!--</el-col>-->
-            <!--<el-col :span="16"></el-col>-->
-            <!--<el-col :span="4">-->
-                <!--<el-button type="primary" style="margin-top: 12px; float: right" @click="next" id="finish">{{finish}}</el-button>-->
-            <!--</el-col>-->
-        <!--</el-row>-->
     </div>
 </template>
 
@@ -106,6 +97,9 @@
                 }
                 else if (this.$route.path === '/new_meeting/step3'){
                     return 2
+                }
+                else if (this.$route.path === '/new_meeting/step4'){
+                    return 3
                 }
             }
         }
